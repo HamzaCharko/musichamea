bf2518fed10e9464084b58139d395428649fe4aa
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-03-26 23:33:32 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,73 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : by Hamza`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : by Hmza' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`By Yasmine. `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+const adminprefix = "$";
+const devs = ['523159342095269898'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+   
+if (message.content.startsWith(adminprefix + 'setP')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setA')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else    
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+ 
+});
+client.on("message", message => {
+    if (message.content === `${prefix}help`) {
+  const embed = new Discord.RichEmbed()
+      .setColor("#000000")
+      .setDescription(`
+**بوت برودكاست**
+${prefix}bc ⇏ برودكاست
+**قوائم للأونر**
+${prefix}setT →تويتش
+${prefix}setA →تغير الصوره
+${prefix}setname → تغير الإسم
+${prefix}setP →بلاينق
+ `)
+   message.channel.sendEmbed(embed)
+   
+   }
+   });
+ 
+ 
+client.login(process.env.BOT_TOKEN);
